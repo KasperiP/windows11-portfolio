3f168e417999ab24d913ffc7dc15fe5856144a8f
kassq <kasperi.pohtine@gmail.com>
2022-01-15 16:54:27 +0200

    fixed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
 	images: {
 		domains: ['res.cloudinary.com'],
 		formats: ['image/avif', 'image/webp'],
-		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-		imageSizes: [256, 512, 1024, 2048, 4096, 8192, 16384, 32768],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+		imageSizes: [256, 512, 1024, 2048, 4096, 8192],
 	},
 };
