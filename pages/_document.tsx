b9ee890697b59cbe9a04200f571580479f12b4a6
Kasperi Pohtinen <kasperi.pohtine@gmail.com>
2023-11-05 21:07:57 +0200

    remove crossorigin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,11 +8,7 @@ export default function Document() {
 			<Head>
 				{/* Fonts */}
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link
-					rel="preconnect"
-					href="https://fonts.gstatic.com"
-					crossOrigin="true"
-				/>
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link
 					href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600&display=swap"
 					rel="stylesheet"
